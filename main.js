1e7bbac6f46d3dfb9872558cb4f85dfac311dd87
OrlandoC017 <orlando.castillo7123@gmail.com>
2025-12-15 22:59:07 -0500

    Almost Done!

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
+
+
 const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.nav__off-screen-menu');
+const menuOverlay = document.querySelector('.menu-overlay');
+const closeBtn = document.querySelector('.btn__menu--close');
 
-hamMenu.addEventListener('click', () => {
-  hamMenu.classList.toggle('active');
-  offScreenMenu.classList.toggle('active');
-});
+function toggleMenu() {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+    menuOverlay.classList.toggle('active');
+}
 
-
-// Close menu when clicking overlay
-menuOverlay.addEventListener('click', () => {
-  hamMenu.classList.remove('active');
-  offScreenMenu.classList.remove('active');
-  menuOverlay.classList.remove('active');
-});
+hamMenu.addEventListener('click', toggleMenu);
+closeBtn.addEventListener('click', toggleMenu);
+menuOverlay.addEventListener('click', toggleMenu);
