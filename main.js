49e2f96b8185c3d40cf1c90e9c9ccbfeb3e76aee
OrlandoC017 <orlando.castillo7123@gmail.com>
2025-12-15 22:44:52 -0500

    Hamburger

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const hamMenu = document.querySelector('.ham-menu')
-const offScreenMenu = document.querySelector('.nav__off-screen-menu')
+const hamMenu = document.querySelector('.ham-menu');
+const offScreenMenu = document.querySelector('.nav__off-screen-menu');
+
 hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-})
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+});
+
+
+// Close menu when clicking overlay
+menuOverlay.addEventListener('click', () => {
+  hamMenu.classList.remove('active');
+  offScreenMenu.classList.remove('active');
+  menuOverlay.classList.remove('active');
+});
